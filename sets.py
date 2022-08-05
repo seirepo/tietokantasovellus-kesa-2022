5c7f650ee0f137e9b3cdc2066a71c3698987814f
susanna <susannae.repo@gmail.com>
2022-08-05 19:38:20 +0300

    Insert new set and card values to db

--- sets.py
+++ sets.py
@@ -1,4 +1,18 @@
-def add_new_set(name, description, words, term, definition, private, id):
-    #TODO: create new set
-    #TODO: create new cards
-    return False
+from db import db
+
+def add_new_set(name, description, words, term, definition, private, creator_id):
+    sql = """INSERT INTO sets (creator_id, name, description, term, definition, private)
+             VALUES (:creator_id, :name, :description, :term, :definition, :private)
+             RETURNING id"""
+    set_id = db.session.execute(sql,
+             {"creator_id":creator_id, "name":name, "description":description,
+             "term":term, "definition":definition, "private":private}).fetchone()[0]
+
+    for row in words.split("\n"):
+        pair = row.strip().split(";")
+        if len(pair) != 2:
+            continue
+        sql = """INSERT INTO cards (set_id, word1, word2)
+                 VALUES (:set_id, :word1, :word2)"""
+        db.session.execute(sql, {"set_id":set_id, "word1":pair[0], "word2":pair[1]})
+    db.session.commit()
