c89b0c2250b1b2ba5c72d918f6f7d81654a43fba
susanna <susannae.repo@gmail.com>
2022-08-05 19:37:06 +0300

    Remove card_order from table sets, reorder rows

--- schema.sql
+++ schema.sql
@@ -7,10 +7,9 @@ CREATE TABLE users (
 
 CREATE TABLE sets (
     id SERIAL PRIMARY KEY,
-    name TEXT,
     creator_id INTEGER REFERENCES users,
+    name TEXT,
     description TEXT,
-    card_order INT,
     term TEXT DEFAULT 'term',
     definition TEXT DEFAULT 'definition',
     private INT
