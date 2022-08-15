11812f549a8249cf457dfdf52b5220fea85c4d2b
susanna <susannae.repo@gmail.com>
2022-08-15 16:43:44 +0300

    Edit schema.sql Add new table for latest games and card results

--- schema.sql
+++ schema.sql
@@ -1,23 +1,38 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    username TEXT,
+    username TEXT UNIQUE,
     password TEXT,
     role INTEGER
 );
 
-CREATE TABLE sets (
+CREATE TABLE IF NOT EXISTS sets (
     id SERIAL PRIMARY KEY,
-    creator_id INTEGER REFERENCES users,
+    creator_id INTEGER REFERENCES users ON DELETE CASCADE,
     name TEXT,
     description TEXT,
     term TEXT DEFAULT 'term',
     definition TEXT DEFAULT 'definition',
-    private INT
+    private INT,
+    creation_time TIMESTAMP
 );
 
-CREATE TABLE cards (
+CREATE TABLE IF NOT EXISTS cards (
     id SERIAL PRIMARY KEY,
     set_id INTEGER REFERENCES sets ON DELETE CASCADE,
     word1 TEXT,
     word2 TEXT
+);
+
+CREATE TABLE IF NOT EXISTS latest_games (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
+    set_id INTEGER REFERENCES sets ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS card_results (
+    id SERIAL PRIMARY KEY,
+    latest_game_id INTEGER REFERENCES latest_games ON DELETE CASCADE,
+    card_id INTEGER REFERENCES cards ON DELETE CASCADE,
+    correctly_guessed INT,
+    times_played INT
 );
