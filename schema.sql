5b445fe6b7420d5da6c2776c116f13030a1e3df2
susanna <susannae.repo@gmail.com>
2022-08-16 13:26:02 +0300

    Edit schema.sql, add NOT NULL constraints

--- schema.sql
+++ schema.sql
@@ -1,26 +1,26 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    username TEXT UNIQUE,
-    password TEXT,
-    role INTEGER
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role INT DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS sets (
     id SERIAL PRIMARY KEY,
     creator_id INTEGER REFERENCES users ON DELETE CASCADE,
-    name TEXT,
+    name TEXT NOT NULL,
     description TEXT,
     term TEXT DEFAULT 'term',
     definition TEXT DEFAULT 'definition',
-    private INT,
-    creation_time TIMESTAMP
+    private INT DEFAULT 0,
+    creation_time TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS cards (
     id SERIAL PRIMARY KEY,
     set_id INTEGER REFERENCES sets ON DELETE CASCADE,
-    word1 TEXT,
-    word2 TEXT
+    word1 TEXT NOT NULL,
+    word2 TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS latest_games (
