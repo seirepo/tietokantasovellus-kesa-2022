d10bd0ea670964f98f97acf479e47309b7c29a5e
susanna <susannae.repo@gmail.com>
2022-08-29 15:24:09 +0300

    Add new row to stats when game is finished

--- stats.py
+++ stats.py
@@ -1,10 +1,21 @@
 from db import db
-from datetime import datetime
+from datetime import datetime, timezone
 import plays
 
 def add_stats(game_id):
     finish_time = datetime.now()
-    game_info = plays.get_game_info(game_id)
-    time = finish_time - game_info.start_time
+    game_info = plays.get_game_info(game_id)[0]
+    time = finish_time - game_info["start_time"]
+    print(str(time))
 
-    sql = """SELECT """
+    print(time, type(time))
+    sql = """SELECT COUNT(*) FROM card_results
+             WHERE latest_game_id=:game_id
+             AND times_guessed_wrong=0"""
+    guessed_on_first = db.session.execute(sql, {"game_id":game_id}).fetchone()[0]
+
+    sql = """INSERT INTO stats (user_id, set_id, play_time, guessed_on_first)
+             VALUES (:user_id, :set_id, :play_time, :guessed_on_first)"""
+
+    db.session.execute(sql, {"user_id":game_info["user_id"], "set_id":game_info["set_id"],
+                            "guessed_on_first":guessed_on_first, "play_time":time})
